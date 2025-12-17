e29b7be32e77dfd585239894d348efabd2125e58
bala-amypo <bala@amypo.in>
2025-12-17 06:26:23 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,60 +1,60 @@
 package com.example.demo.entity;
+
 import java.time.LocalDate;
+
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 @Entity
-public class Student{
-@Id
-@GeneratedValue(strategy=GenerationType.IDENTITY)
-private Long id;
-private String name;
-private String email;
-private LocalDate dob;
-private float cgpa;
-public Long getId() {
-    return id;
-}
-public void setId(Long id) {
-    this.id = id;
-}
-public String getName() {
-    return name;
-}
-public void setName(String name) {
-    this.name = name;
-}
-public String getEmail() {
-    return email;
-}
-public void setEmail(String email) {
-    this.email = email;
-}
-public LocalDate getDob() {
-    return dob;
-}
-public void setDob(LocalDate dob) {
-    this.dob = dob;
-}
-public float getCgpa() {
-    return cgpa;
-}
-public void setCgpa(float cgpa) {
-    this.cgpa = cgpa;
-}
-public Student(int id, String name, String dept) {         this.id = id;
-         this.name = name;
-       this.dept = dept;
-    }
-     public Student(int id, String name, String dept) {
-        this.id = id;
-        this.name = name;
-        this.email= email;
-         this.dob= dob;
-          this.cgpa= cgpa;
-       
-    
-    }
 
+public class Student{
+    @Id
+@GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private LocalDate dob;
+    private String email;
+    private float cgpa;
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public LocalDate getDob() {
+        return dob;
+    }
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public float getCgpa() {
+        return cgpa;
+    }
+    public void setCgpa(float cgpa) {
+        this.cgpa = cgpa;
+    }
+   public Student(Long id, String name, String email, LocalDate dob, float cgpa) {
+    this.id = id;
+    this.name = name;
+    this.email=email;
+    this.dob=dob;
+    this.cgpa=cgpa;
+}
+
+    
+    
 }
