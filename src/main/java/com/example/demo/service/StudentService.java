3516db09c1afc9995ab527fbe0b93c4fa730989d
bala-amypo <bala@amypo.in>
2025-12-17 08:26:14 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,9 +1,13 @@
-package com.example.demo.service.impl;
+package com.example.demo.service;
+
 import java.util.List;
 import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+
 import com.example.demo.entity.Student;
-import com.example.demo.repository.StudentRepo;
-import com.example.demo.service.StudentService;
-@Service
+
+public interface StudentService {
+    Student insertStudent(Student st);
+    List<Student> getAllStudents();
+    Optional<Student> getOneStudent(Long id);
+    void deleteStudent(Long id);
+}
