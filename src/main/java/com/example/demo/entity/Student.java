170ba3aae4f1426a1796020a25b1acee92e362d1
bala-amypo <bala@amypo.in>
2025-12-17 06:18:18 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -7,3 +7,55 @@ import jakarta.persistence.Id;
 @Entity
 @Id
 @GeneratedValue(strategy=GenerationType.IDENTITY)
+public class Student{
+
+private Long id;
+private String name;
+private String email;
+private LocalDate dob;
+private float cgpa;
+public Long getId() {
+    return id;
+}
+public void setId(Long id) {
+    this.id = id;
+}
+public String getName() {
+    return name;
+}
+public void setName(String name) {
+    this.name = name;
+}
+public String getEmail() {
+    return email;
+}
+public void setEmail(String email) {
+    this.email = email;
+}
+public LocalDate getDob() {
+    return dob;
+}
+public void setDob(LocalDate dob) {
+    this.dob = dob;
+}
+public float getCgpa() {
+    return cgpa;
+}
+public void setCgpa(float cgpa) {
+    this.cgpa = cgpa;
+}
+public Studentity(int id, String name, String dept) {         this.id = id;
+         this.name = name;
+       this.dept = dept;
+    }
+     public Studentity(int id, String name, String dept) {
+        this.id = id;
+        this.name = name;
+        this.email= email;
+         this.dob= dob;
+          this.cgpa= cgpa;
+       
+    
+    }
+
+}
